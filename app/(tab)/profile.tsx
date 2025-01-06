4043f2240148af48b6ccdc877cb46fcbc5733ff4
manojmksmanu <manoj2022019@gmail.com>
2025-01-06 13:21:40 +0530

    create dummy page link on profile page for static pages

--- app/(tab)/profile.tsx
+++ app/(tab)/profile.tsx
@@ -1,9 +1,114 @@
-import { View, Text } from "react-native";
+import { DescriptionText, Headline3 } from "@/components/atoms/Text";
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { useTheme } from "@/context/ThemeContext";
+import { Link } from "expo-router";
 
 export default function ProfileTab() {
+  const theme = useTheme();
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Profile Tab Screen</Text>
-    </View>
+    <ScrollView style={{ flex: 1 }}>
+      {/* User Info Container */}
+      <View style={{ height: 300, backgroundColor: "blue", width: "100%" }} />
+
+      {/* Public Access Page Link */}
+
+      <Link href="/pages/about">
+        <View
+          style={[
+            styles.linkContainer,
+            {
+              borderColor: theme.lightText,
+            },
+          ]}
+        >
+          <Headline3 text="About us" />
+          <AntDesign name="right" size={18} color={theme.text} />
+        </View>
+      </Link>
+      <Link href="/pages/about">
+        <View
+          style={[
+            styles.linkContainer,
+            {
+              borderColor: theme.lightText,
+            },
+          ]}
+        >
+          <Headline3 text="About us" />
+          <AntDesign name="right" size={18} color={theme.text} />
+        </View>
+      </Link>
+      <Link href="/pages/about">
+        <View
+          style={[
+            styles.linkContainer,
+            {
+              borderColor: theme.lightText,
+            },
+          ]}
+        >
+          <Headline3 text="About us" />
+          <AntDesign name="right" size={18} color={theme.text} />
+        </View>
+      </Link>
+      <Link href="/pages/about">
+        <View
+          style={[
+            styles.linkContainer,
+            {
+              borderColor: theme.lightText,
+            },
+          ]}
+        >
+          <Headline3 text="About us" />
+          <AntDesign name="right" size={18} color={theme.text} />
+        </View>
+      </Link>
+      <Link href="/pages/about">
+        <View
+          style={[
+            styles.linkContainer,
+            {
+              borderColor: theme.lightText,
+            },
+          ]}
+        >
+          <Headline3 text="About us" />
+          <AntDesign name="right" size={18} color={theme.text} />
+        </View>
+      </Link>
+
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          marginVertical: 18,
+        }}
+      >
+        <DescriptionText text="Version 1.1 LeleKart" />
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  linkContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
+    paddingVertical: 18,
+    borderBottomWidth: 0.5,
+    width: "100%",
+  },
+});
--- components/atoms/Text.tsx
+++ components/atoms/Text.tsx
@@ -8,87 +8,85 @@ type HeadlineProps = {
 export function Headline({ text }: HeadlineProps) {
   const theme = useTheme();
   return (
-    <View style={{}}>
       <Text style={{ fontSize: 34, fontWeight: "bold", color: theme.text }}>
         {text}
       </Text>
-    </View>
   );
 }
 
 export function Headline2({ text }: HeadlineProps) {
   const theme = useTheme();
   return (
-    <View style={{}}>
       <Text style={{ fontSize: 24, fontWeight: 600, color: theme.text }}>
         {text}
       </Text>
-    </View>
   );
 }
 
 export function Headline3({ text }: HeadlineProps) {
   const theme = useTheme();
   return (
-    <View style={{}}>
-      <Text style={{ fontSize: 18, fontWeight: 600, color: theme.text }}>
+    // <View style={{}}>
+      <Text style={{ fontSize: 20, fontWeight: 500, color: theme.text }}>
         {text}
       </Text>
-    </View>
+    // </View>
   );
 }
 
 export function Subheads({ text }: HeadlineProps) {
   const theme = useTheme();
   return (
-    <View style={{}}>
-      <Text style={{ fontSize: 16, fontWeight: 600, color: theme.lightText }}>
+    // <View style={{}}>
+      <Text style={{ fontSize: 16, fontWeight: 600, color: theme.text }}>
         {text}
       </Text>
-    </View>
+    // </View>
   );
 }
 
 export function NormalText({ text }: HeadlineProps) {
   const theme = useTheme();
   return (
-    <View style={{}}>
+    // <View style={{}}>
       <Text style={{ fontSize: 16, fontWeight: "regular", color: theme.text }}>
         {text}
       </Text>
-    </View>
+    // </View>
   );
 }
 
 export function DescriptiveItems({ text }: HeadlineProps) {
   const theme = useTheme();
   return (
-    <View style={{}}>
+    // <View style={{}}>
       <Text style={{ fontSize: 16, fontWeight: "medium", color: theme.text }}>
         {text}
       </Text>
-    </View>
+    // </View>
   );
 }
 
 export function DescriptionText({ text }: HeadlineProps) {
   const theme = useTheme();
   return (
-    <View style={{}}>
+    // <View style={{}}>
       <Text style={{ fontSize: 14, fontWeight: "regular", color: theme.text }}>
         {text}
       </Text>
-    </View>
+    // </View>
   );
 }
 
 export function HelperText({ text }: HeadlineProps) {
   const theme = useTheme();
   return (
-    <View style={{}}>
-      <Text style={{ fontSize: 12, fontWeight: "regular", color: theme.lightText }}>
+    // <View style={{}}>
+      <Text
+        style={{ fontSize: 12, fontWeight: "regular", color: theme.lightText }}
+      >
         {text}
       </Text>
-    </View>
+    // </View>
   );
 }
